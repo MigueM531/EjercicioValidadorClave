99c6d91d0637b4570fa949af2050d8a7732b0935
Migue <sapotico1234@gmail.com>
2024-10-16 13:37:37 -0500

    wuoooooh created classes and methods but contains pass

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,31 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import abstractclassmethod, abstractmethod
+
+@abstractclassmethod
+class ReglaValidacion:
+
+    def __init__(self, longitud_esperada: int):
+        self._longitud_esperada: int = longitud_esperada
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+    def _validar_longitud(self, clave: str) -> bool:
+            pass
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        pass
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        pass
+
+    def _contiene_numero(self, clave: str) -> bool:
+        pass
+
+
+class Validador:
+    def __init__(self, regla: ReglaValidacion):
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        pass
